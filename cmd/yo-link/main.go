9e6c3da715c9e3e8cf1083f32df565ce8b6a4255
minxrka <minorka04@mail.ru>
2025-05-03 02:50:34 +0300

    *main

--- cmd/yo-link/main.go
+++ cmd/yo-link/main.go
@@ -2,16 +2,40 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/yowie645/Yo-Link/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 	fmt.Println(cfg)
 
-	//TODO: init logger : slog import "log/slog"
+	log := setupLogger(cfg.Env)
+	log.Info("starting", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 	//TODO: init storage : sqlite
 	//TODO: init router : chi, "chi render"
 	//TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
