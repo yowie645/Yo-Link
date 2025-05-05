959d086c63836a945b47f87d57fe1be47c96ce26
minxrka <minorka04@mail.ru>
2025-05-05 19:29:59 +0300

    *logger

--- internal/https-server/middleware/logger/logger.go
+++ internal/https-server/middleware/logger/logger.go
@@ -11,13 +11,15 @@ import (
 
 func New(log *slog.Logger) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
+
 		log := log.With(
 			slog.String("component", "middleware/logger"),
 		)
 
-		log.Info("logger middleware enabled")
+		log.Info("logger middleware initialized")
 
 		fn := func(w http.ResponseWriter, r *http.Request) {
+
 			entry := log.With(
 				slog.String("method", r.Method),
 				slog.String("path", r.URL.Path),
@@ -25,14 +27,17 @@ func New(log *slog.Logger) func(next http.Handler) http.Handler {
 				slog.String("user_agent", r.UserAgent()),
 				slog.String("request_id", middleware.GetReqID(r.Context())),
 			)
-			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
 
-			t1 := time.Now()
+			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+			startTime := time.Now()
+
 			defer func() {
+
 				entry.Info("request completed",
 					slog.Int("status", ww.Status()),
 					slog.Int("bytes", ww.BytesWritten()),
-					slog.String("duration", time.Since(t1).String()),
+					slog.Duration("duration_ms", time.Since(startTime)),
+					slog.Float64("duration_sec", time.Since(startTime).Seconds()),
 				)
 			}()
 
